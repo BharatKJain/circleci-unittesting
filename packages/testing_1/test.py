553573c788a471c7cc156c1e4ea95bf47034ad1a
SunainaL <sunaina.lalwani@guidion.net>
2020-07-13 19:29:40 +0530

    Updated

--- packages/testing_1/test.py
+++ packages/testing_1/test.py
@@ -14,7 +14,7 @@ class TestStringMethods(unittest.TestCase):
 
     def test_split(self):
         s = 'hello world'
-        self.assertEqual(s.split(), ['hel', 'world'])
+        self.assertEqual(s.split(), ['hello', 'world'])
         # check that s.split fails when the separator is not a string
         with self.assertRaises(TypeError):
             s.split(2)
